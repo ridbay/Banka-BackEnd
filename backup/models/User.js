b0748a7e17c4f96f418dcb855eb0e9f1e4d56989
Ridwan Balogun <balogunridwan@gmail.com>
2020-08-08 14:57:03 +0100

    Backend server

---
+++ backup/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+// const userSchema = new Schema({
+//     firstName: String,
+//     lastName: String,
+//     email: String,
+//     password: String,
+//     permissionLevel: Number
+//  });
+let userSchema = new Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email:{
+        type:String,
+        unique: true
+    },
+    password:{
+        type: String
+    }
+}, {collection: 'users'});
+
+userSchema.plugin(uniqueValidator, {message: 'Email already in use.'});
+
+module.exports = mongoose.model("User", userSchema);
