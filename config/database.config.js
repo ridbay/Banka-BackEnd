4bf91474801d9e9ef1d2e7027450679370e1c093
Balogun Ridwan Ridbay <26006774+ridbay@users.noreply.github.com>
2020-09-05 14:59:50 +0100

    Deploying to cpanel

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/banka-db'
-}
+  local: "mongodb://localhost:27017/banka-db",
+  connection:
+    "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority",
+};
